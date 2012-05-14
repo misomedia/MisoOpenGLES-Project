73d084485af1938b325355462c548600d880e415
Ryan Hiroaki Tsukamoto <ryantsukamoto@Ryan-Tsukamotos-iMac.local>
2012-05-14 10:10:14 -0700

    ...using a different method for constructing frameworks containing submodules...

---
+++ MisoOpenGLES/MisoOpenGLES/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MisoOpenGLES
+//
+//  Created by Ryan Tsukamoto on 5/14/12.
+//  Copyright (c) 2012 Miso Media. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
