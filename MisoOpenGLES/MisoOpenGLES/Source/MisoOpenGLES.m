c94d3eca1ce1b27e6b13db5753550c013739c628
Ryan Hiroaki Tsukamoto <ryantsukamoto@Ryan-Tsukamotos-iMac.local>
2012-05-14 13:12:41 -0700

    ...fucking kill me...

--- MisoOpenGLES/MisoOpenGLES/Source/MisoOpenGLES.m
+++
@@ -1,9 +0,0 @@
-//
-//  MisoOpenGLES.m
-//  MisoOpenGLES
-//
-//  Created by Ryan Tsukamoto on 5/4/12.
-//  Copyright (c) 2012 Miso Media. All rights reserved.
-//
-
-#import <MisoOpenGLES/MisoOpenGLES.h>
